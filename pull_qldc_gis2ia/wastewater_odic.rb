a116f68b49d22980971679516fed569fa3cc10fb
Hywel Lewis <Hywel.Lewis@qldc.govt.nz>
2024-09-11 17:11:30 +1200

    batch update

--- pull_qldc_gis2ia/wastewater_odic.rb
+++ pull_qldc_gis2ia/wastewater_odic.rb
@@ -1,40 +1,35 @@
-begin
+# wastewater_odic.rb
+# ===========================================================================================
+#
 
-def print_names(folder = 'c:\github\qldc-pi-ia-iam-prod\pull_qldc_gis2ia')
-  puts folder
-end
+folder = 'C:\Github\qldc-pi-ia-iam-prod\pull_qldc_gis2ia'
 
-nw=WSApplication.current_network
+net=WSApplication.current_network
 
-## Set options for import
-options=Hash.new													## Type | Default | Notes
-#options['Error File'] = folder + '\temp\wastewater_error_log.txt'	## String | blank | Path of error file
-#options['Callback Class'] = ImporterClass							## String | blank | Class used for Ruby callback methods (ICM & InfoAsset only)
-#options['Set Value Flag'] = 'GDB'									## String | blank | Flag used for fields set from data
-#options['Default Value Flag'] = 'GDB'								## String | blank | Flag used for fields set from the default value column
-#options['Image Folder'] = 'C:\Temp\'								## String | blank | Folder to import images from (Asset networks only)
-#options['Duplication Behaviour'] = 'Merge'							## String | Merge | One of Duplication Behaviour:'Overwrite','Merge','Ignore'
-#options['Units Behaviour'] = 'Native'								## String | Native | One of 'Native','User','Custom'
-#options['Update Based On Asset ID'] = false						## Boolean | false
-#options['Update Only'] = false										## Boolean | false
-#options['Delete Missing Objects'] = false							## Boolean | false
-#options['Allow Multiple Asset IDs'] = false						## Boolean | false
-#options['Update Links From Points'] = false						## Boolean | false
-#options['Blob Merge'] = false										## Boolean | false
-#options['Use Network Naming Conventions'] = false					## Boolean | false
-#options['Import images'] = false									## Boolean | false | Asset networks only
-#options['Group Type'] = false										## Boolean | false | Asset networks only
-#options['Group Name'] = false										## Boolean | false | Asset networks only
+puts 'Start import'
+
+# Set up params
+csv_options=Hash.new
+csv_options['Use Display Precision'] = false
+csv_options['Flag Fields '] = false
+csv_options['Multiple Files'] = true
+csv_options['Selection Only'] = false
+csv_options['Coordinate Arrays Format'] = 'Packed'
+csv_options['Other Arrays Format'] = 'Separate'
+csv_options['WGS84'] = false
+csv_options['Duplication Behaviour'] = 'Overwrite'
+csv_options['Delete Missing Objects'] = true
+csv_options['Update Links From Points'] = false
+csv_options['Default Value Flag'] = '#S'
+csv_options['Set Value Flag'] = '#A'
 
 ## Action the Import using odic_import_ex
-nw.odic_import_ex('GDB',
-'C:\Github\qldc-pi-ia-iam-prod\pull_qldc_gis2ia\wastewater_config.cfg',
-options,'node','NodeClass',
-'C:\Github\qldc-pi-ia-iam-prod\pull_qldc_gis2ia\wastewater.gdb')
+net.odic_import_ex('CSV',
+	folder + '\wastewater_config.cfg',
+	csv_options, 'node',
+	folder + '\exports\wwManhole.csv'
+	)
+puts 'wwManhole import completed'
 
-nw.odic_import_ex('GDB',
-'C:\Github\qldc-pi-ia-iam-prod\pull_qldc_gis2ia\wastewater_config.cfg',
-options,'pipe','PipeClass',
-'C:\Github\qldc-pi-ia-iam-prod\pull_qldc_gis2ia\wastewater.gdb')
-
-puts 'Import from GDB completed'
+# clear selection
+net.clear_selection
